2921ccbc2c8d402f481d87ef2619bdf0a50f67f3
yehor393 <134985773+yehor393@users.noreply.github.com>
2024-11-23 23:48:13 -0700

    First successful reading
    Field: CX1-480180-100-T2A-IIB-T (Trust: 0.55)
    
    Field: RH266382 (Trust: 0.76)

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
 import cv2
+import easyocr
 from ultralytics import YOLO
 
-# load models
-coco_model = YOLO('yolo8n.pt')
-model_serial_detector = YOLO('model_serial_detector.pt')
+# Завантаження моделі YOLO
+model = YOLO("models/last.pt")
 
-# Read frames
-cap = cv2.VideoCapture('./video.mp4')
+# Передбачення на зображенні
+results = model.predict(source=r"C:\Users\yehor\Documents\GitHub\TesterReport\data\images\test_image.jpg", conf=0.25, imgsz=640)
 
-frame_num = -1
-ret = True
-while ret:
-    frame_num += 1
-    ret, frame = cap.read()
-    if ret and frame_num < 10:
-        # detect dataplate
-        detections = coco_model(frame)[0]
-        print(detections)
+# Завантаження зображення
+image = cv2.imread(r"C:\Users\yehor\Documents\GitHub\TesterReport\data\images\test_image.jpg")
+
+# Ініціалізація EasyOCR
+reader = easyocr.Reader(['en'])  # Можна додати інші мови за потреби
+
+# Ітерування по знайдених полях
+for result in results:
+    for box in result.boxes.data.tolist():
+        x_min, y_min, x_max, y_max = map(int, box[:4])
+
+        # Обрізання області поля
+        cropped_img = image[y_min:y_max, x_min:x_max]
+
+        # Перетворення в градації сірого та бінаризація
+        gray_img = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2GRAY)
+        _, binary_img = cv2.threshold(gray_img, 184, 255, cv2.THRESH_BINARY_INV)
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 2))
+        binary_img = cv2.morphologyEx(binary_img, cv2.MORPH_CLOSE, kernel)
+
+        # Використання EasyOCR для зчитування тексту з очищеного зображення
+        ocr_result = reader.readtext(binary_img)
+
+        # Виведення результатів OCR
+        text_combined = ""
+        for detection in ocr_result:
+            _, text, confidence = detection
+            text_combined += text.strip() + " "
+            print(f"Знайдене поле: {text.strip()} (довіра: {confidence:.2f})")
+
+        
+        # Перегляд бінаризованого зображення
+        cv2.imshow("Binary Image", binary_img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
