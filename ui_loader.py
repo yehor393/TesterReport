0922a2ed6dbac69ef1b25beb39e0bdb7a029b846
yehor393 <134985773+yehor393@users.noreply.github.com>
2024-11-30 21:48:36 -0700

    fix/tabs_and_checkbox
    Fixed the issue with cleaning after switching between different models. Also fixed the bug with the 'save' checkbox staying checked after changing the model multiple times.

--- ui_loader.py
+++ ui_loader.py
@@ -1,7 +1,6 @@
 from PyQt5.QtWidgets import QMainWindow, QLineEdit, QCheckBox, QTabWidget, QPushButton
 from PyQt5.uic import loadUi
 from pdf_generator import generate_pdf_report
-from set_tabs import set_tabs_for_model
 
 class HeaterTestApp(QMainWindow):
     def __init__(self):
@@ -13,22 +12,14 @@ class HeaterTestApp(QMainWindow):
 
         # Отримання доступу до елементів інтерфейсу
         self.tab_widget: QTabWidget = self.findChild(QTabWidget, "tabWidget")
-        self.model_input: QLineEdit = self.findChild(QLineEdit, "model_input")  # Поле моделі
+
         self.serial_input: QLineEdit = self.findChild(QLineEdit, "serial_input")  # Поле серійного номера
-        self.checkbox_list = self.findChildren(QCheckBox)
+        self.model_input: QLineEdit = self.findChild(QLineEdit, "line_model")  # Замініть "modelInput" на ім'я вашого QLineEdit
+
         self.post_button: QPushButton = self.findChild(QPushButton, "post_button")  # Кнопка для створення PDF
-        self.clear_button: QPushButton = self.findChild(QPushButton, "clear_button")  # Кнопка для очищення
-        self.fill_button: QPushButton = self.findChild(QPushButton, "fill_button")
 
         # Підключення кнопок до функцій
-        self.clear_button.clicked.connect(self.clear_all_fields)
-        self.fill_button.clicked.connect(self.clear_all_fields)
         self.post_button.clicked.connect(self.generate_pdf)
-
-
-        # Отримуємо доступ до вкладок (QTabWidget) та поля введення (QLineEdit)
-        self.tab_widget: QTabWidget = self.findChild(QTabWidget, "tabWidget")  # Замініть "tabWidget" на ім'я вашого QTabWidget
-        self.model_input: QLineEdit = self.findChild(QLineEdit, "line_model")  # Замініть "modelInput" на ім'я вашого QLineEdit
 
         # Підключаємо подію введення тексту
         self.model_input.editingFinished.connect(self.on_model_entered)
@@ -43,6 +34,106 @@ class HeaterTestApp(QMainWindow):
         self.clear_tabs()
 
 
+    def on_model_entered(self):
+        """Обробляємо подію після введення моделі."""
+        model = self.model_input.text().strip()  # Отримуємо текст із поля введення
+
+        if model:  # Перевіряємо, чи введено модель
+            self.set_tabs_for_model(model)
+        else:  # Якщо поле порожнє
+            self.clear_tabs() 
+
+
+    def set_tabs_for_model(self, model):
+        """Додає потрібні вкладки залежно від моделі."""
+        self.clear_all_checkboxes()
+        self.clear_tabs()
+        if model == "FX6":
+            self.tab_widget.addTab(self.outside_steam, "Outside")
+            self.tab_widget.addTab(self.value, "Value")
+            self.tab_widget.addTab(self.final, "Final")
+        elif model == "CX1":
+            self.tab_widget.addTab(self.outside_convector, "Outside")
+            self.tab_widget.addTab(self.value, "Value")
+            self.tab_widget.addTab(self.final, "Final")
+        else:
+            self.clear_tabs()
+            
+        self.initialize_active_tab()
+
+
+    def initialize_active_tab(self):
+        """Ініціалізація всіх вкладок після введення моделі."""
+        self.checkbox_lists = {}  # Словник для чекбоксів кожної вкладки
+
+        for index in range(self.tab_widget.count()):
+            tab = self.tab_widget.widget(index)
+            
+            # Знаходимо чекбокси тільки у поточній вкладці
+            self.checkbox_lists[index] = tab.findChildren(QCheckBox)
+
+            # Знайти всі кнопки на вкладці
+            buttons = tab.findChildren(QPushButton)
+
+            # Обробити кнопки за іменами
+            for button in buttons:
+                    
+                    try:
+                        button.clicked.disconnect()
+                    except TypeError:
+                        pass
+                    
+                    if "clear_button" in button.objectName():
+                        button.clicked.connect(lambda _, b=button: self.clear_fields(b))
+                    elif "fill_button" in button.objectName():
+                        button.clicked.connect(lambda _, b=button: self.fill_fields(b))
+
+    def clear_all_checkboxes(self):
+        """Очищує всі чекбокси на всіх вкладках."""
+        for index in range(self.tab_widget.count()):
+            tab = self.tab_widget.widget(index)
+            checkboxes = tab.findChildren(QCheckBox)
+            for checkbox in checkboxes:
+                checkbox.setChecked(False)
+                
+    def clear_fields(self, button):
+        """Очищення полів, пов'язаних із кнопкою."""
+        tab = self.get_parent_tab(button)  # Отримуємо вкладку
+        if tab:  # Перевіряємо, чи вкладка знайдена
+            checkboxes = tab.findChildren(QCheckBox)  # Знаходимо чекбокси у вкладці
+            for checkbox in checkboxes:
+                checkbox.setChecked(False)
+
+
+    def fill_fields(self, button):
+        """Заповнення полів, пов'язаних із кнопкою."""
+        tab = self.get_parent_tab(button)  # Отримуємо вкладку
+        if tab:  # Перевіряємо, чи вкладка знайдена
+            checkboxes = tab.findChildren(QCheckBox)  # Знаходимо чекбокси у вкладці
+            for checkbox in checkboxes:
+                checkbox.setChecked(True)
+            # Повернути фокус на кнопку
+        button.setFocus()
+
+    def get_parent_tab(self, widget):
+        """Отримати вкладку, якій належить віджет."""
+        while widget and widget not in [self.tab_widget.widget(i) for i in range(self.tab_widget.count())]:
+            widget = widget.parent()
+        return widget
+    
+
+    def clear_all_fields(self, index):
+        """Очищення чекбоксів у заданій вкладці."""
+        for checkbox in self.checkbox_lists.get(index, []):
+            checkbox.setChecked(False)
+
+
+    def fill_all_fields(self, index):
+        """Заповнення чекбоксів у заданій вкладці."""
+        for checkbox in self.checkbox_lists.get(index, []):
+            checkbox.setChecked(True)
+
+
     def generate_pdf(self):
         """Збір даних та виклик функції створення PDF."""
         # Збір даних
@@ -54,24 +145,8 @@ class HeaterTestApp(QMainWindow):
         # Виклик функції для створення PDF
         generate_pdf_report(model, serial, checkbox_results)
 
+
     def clear_tabs(self):
         """Видаляє всі вкладки."""
         while self.tab_widget.count() > 0:
             self.tab_widget.removeTab(0)
-
-    def on_model_entered(self):
-        """Обробляємо подію після введення моделі."""
-        model = self.model_input.text().strip()  # Отримуємо текст із поля введення
-
-        if model:  # Перевіряємо, чи введено модель
-            self.set_tabs_for_model(model)
-
-    def clear_all_fields(self):
-        # Очищення чекбоксів
-        for checkbox in self.checkbox_list:
-            checkbox.setChecked(False)
-
-    def fill_all_fields(self):
-        # Заповнення чекбоксів
-        for checkbox in self.checkbox_list:
-            checkbox.setChecked(True)
